26e1575660405a334fdcf789e81b7d58e22ce869
leopold9707 <rkqcjs123@gmail.com>
2020-08-19 02:06:57 +0900

    upgrade to version 2

--- aws-ec2-by-hostname/010-provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-### terraform cloud에서 실행할 경우 미리 Environment Variables에 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_REGION'을 설정해야 함.
-  region     = "ap-northeast-2"
-  shared_credentials_file = "~/.aws/credentials"
-}
